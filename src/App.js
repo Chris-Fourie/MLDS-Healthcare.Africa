57c9f20a0aebeb3d527b60fd9ff0c25f02297252
Chris Fourie <chrisfourie.sa@gmail.com>
2019-08-28 11:51:09 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,6 +13,14 @@ class App extends Component {
         <p className="App-intro">
         <a href="https://groups.google.com/forum/#!forum/mlds-healthcare-africa">Join the mailing list =) </a>         
         </p>
+    
+        <p className="App-intro">
+        <a href="https://groups.google.com/forum/#!forum/mlds-healthcare-africa">Join project - Anatomical Pathology - Ectopic Classification </a>
+        </p>
+      
+        <p className="App-intro">
+        <a href="https://groups.google.com/forum/#!forum/mlds-healthcare-africa">Join project - Haematology - Acute Luekaemia Diagnosis Pipeline </a>
+        </p>
         
       
       </div>
