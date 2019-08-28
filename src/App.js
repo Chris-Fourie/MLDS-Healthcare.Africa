08ba8caabd17b54c444257ede9dded1fec30bb69
Chris Fourie <chrisfourie.sa@gmail.com>
2019-08-28 10:07:37 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+var txt = "MLDS-Healthcare-Africa mailing list";
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to MLDS-Healthcare-Africa</h2>
         </div>
+        <p>  txt.link("https://groups.google.com/forum/#!forum/mlds-healthcare-africa") + "</p>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          
         </p>
       </div>
     );
